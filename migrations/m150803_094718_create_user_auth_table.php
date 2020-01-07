601d90e8c1c25fced8b22ceac87c2c022974c71e
David Große <david.grosse@haendlerbund.de>
2020-01-07 08:06:49 +0100

    use better table names in migrations

--- migrations/m150803_094718_create_user_auth_table.php
+++ migrations/m150803_094718_create_user_auth_table.php
@@ -7,36 +7,44 @@ class m150803_094718_create_user_auth_table extends Migration
 {
     public function safeUp()
     {
-        $this->createTable('{{%user}}', [
-            'id' => Schema::TYPE_PK,
-            'username' => Schema::TYPE_STRING . ' NOT NULL',
-            'auth_key' => Schema::TYPE_STRING . '(32) NOT NULL',
-            'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
+        $this->createTable('{{%auth0_users}}', [
+            'id'                   => Schema::TYPE_PK,
+            'username'             => Schema::TYPE_STRING . ' NOT NULL',
+            'auth_key'             => Schema::TYPE_STRING . '(32) NOT NULL',
+            'password_hash'        => Schema::TYPE_STRING . ' NOT NULL',
             'password_reset_token' => Schema::TYPE_STRING,
-            'email' => Schema::TYPE_STRING . ' NOT NULL',
+            'email'                => Schema::TYPE_STRING . ' NOT NULL',
 
-            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'status'     => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
             'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
         ]);
 
-        $this->createTable('{{%auth}}', [
-            'id'    			   	=> Schema::TYPE_PK,
-            'user_id'       	   	=> Schema::TYPE_INTEGER . ' NOT NULL',
-            'source'	   			=> Schema::TYPE_STRING . '(255) NOT NULL',
-            'source_id'	   			=> Schema::TYPE_STRING . '(255) NOT NULL',
+        $this->createTable('{{%auth0_auth}}', [
+            'id'        => Schema::TYPE_PK,
+            'user_id'   => Schema::TYPE_INTEGER . ' NOT NULL',
+            'source'    => Schema::TYPE_STRING . '(255) NOT NULL',
+            'source_id' => Schema::TYPE_STRING . '(255) NOT NULL',
         ]);
 
         /* BEGIN relationship */
-		$this->addForeignKey('fk_user_auth', '{{%auth}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'RESTRICT');
-		/* END relationship */
+        $this->addForeignKey(
+            'fk_user_auth',
+            '{{%auth0_auth}}',
+            'user_id',
+            '{{%auth0_users}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+        /* END relationship */
     }
 
     public function safeDown()
     {
-        $this->dropForeignKey('fk_user_auth', '{{%auth}}');
+        $this->dropForeignKey('fk_user_auth', '{{%auth0_auth}}');
 
-        $this->dropTable('{{%user}}');
-        $this->dropTable('{{%auth}}');
+        $this->dropTable('{{%auth0_users}}');
+        $this->dropTable('{{%auth0_users}}');
     }
 }
--- migrations/m150805_092256_create_tenant_tables.php
+++ migrations/m150805_092256_create_tenant_tables.php
@@ -10,42 +10,63 @@ class m150805_092256_create_tenant_tables extends Migration
     public function safeUp()
     {
         /* BEGIN tenant */
-		$this->createTable('{{%tenant}}', [
-			'id'    			   => Schema::TYPE_PK,
-			'name'  			   => Schema::TYPE_STRING . '(255) NOT NULL',
-			'created_at'           => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-			'create_user_id'  	   => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-            'updated_at'           => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-			'update_user_id'  	   => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-		]);
-		/* END tenant */
+        $this->createTable('{{%auth0_tenants}}', [
+            'id'             => Schema::TYPE_PK,
+            'name'           => Schema::TYPE_STRING . '(255) NOT NULL',
+            'created_at'     => Schema::TYPE_DATETIME . ' DEFAULT NULL',
+            'create_user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+            'updated_at'     => Schema::TYPE_DATETIME . ' DEFAULT NULL',
+            'update_user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+        ]);
+        /* END tenant */
 
-		/* BEGIN tenant_user */
-		$this->createTable('{{%tenant_user}}', [
-			'id'    			   => Schema::TYPE_PK,
-			'tenant_id'  		   => Schema::TYPE_INTEGER . ' NOT NULL',
-			'user_id'  			   => Schema::TYPE_INTEGER . ' NOT NULL',
-			'created_at'           => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-			'create_user_id'  	   => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-            'updated_at'           => Schema::TYPE_DATETIME . ' DEFAULT NULL',
-			'update_user_id'  	   => Schema::TYPE_INTEGER . ' DEFAULT NULL',
-		]);
-		/* END tenant_user */
+        /* BEGIN tenant_user */
+        $this->createTable('{{%auth0_tenants_users}}', [
+            'id'             => Schema::TYPE_PK,
+            'tenant_id'      => Schema::TYPE_INTEGER . ' NOT NULL',
+            'user_id'        => Schema::TYPE_INTEGER . ' NOT NULL',
+            'created_at'     => Schema::TYPE_DATETIME . ' DEFAULT NULL',
+            'create_user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+            'updated_at'     => Schema::TYPE_DATETIME . ' DEFAULT NULL',
+            'update_user_id' => Schema::TYPE_INTEGER . ' DEFAULT NULL',
+        ]);
+        /* END tenant_user */
 
         /* BEGIN relationship */
-		$this->addForeignKey('fk_tenant_tenant_user', '{{%tenant_user}}', 'tenant_id', '{{%tenant}}', 'id', 'CASCADE', 'RESTRICT');
-        $this->addForeignKey('fk_user_tenant_user', '{{%tenant_user}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'RESTRICT');
-		/* END relationship */
+        $this->addForeignKey(
+            'fk_tenant_tenant_user',
+            '{{%auth0_tenants_users}}',
+            'tenant_id',
+            '{{%auth0_tenants}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+        $this->addForeignKey(
+            'fk_user_tenant_user',
+            '{{%auth0_tenants_users}}',
+            'user_id',
+            '{{%auth0_users}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+        /* END relationship */
 
         /* BEGIN index */
-        $this->createIndex('ix_tenant_name', '{{%tenant}}', 'name', true);
+        $this->createIndex(
+            'ix_tenant_name',
+            '{{%auth0_tenants}}',
+            'name',
+            true
+        );
     }
 
     public function safeDown()
     {
-        $this->dropForeignKey('fk_tenant_tenant_user', '{{%tenant_user}}');
-        $this->dropForeignKey('fk_user_tenant_user', '{{%tenant_user}}');
-		$this->dropTable('{{%tenant}}');
-		$this->dropTable('{{%tenant_user}}');
+        $this->dropForeignKey('fk_tenant_tenant_user', '{{%auth0_tenants_users}}');
+        $this->dropForeignKey('fk_user_tenant_user', '{{%auth0_tenants_users}}');
+        $this->dropTable('{{%auth0_tenants}}');
+        $this->dropTable('{{%auth0_tenants_users}}');
     }
 }
