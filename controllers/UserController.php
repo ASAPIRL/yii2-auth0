2e3ab5d68e021731e3bba0d8a24c270cdf086688
Herbert Maschke <thyseus@pm.me>
2020-01-09 13:37:23 +0100

    add some events

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -7,6 +7,7 @@
 
 namespace thyseus\auth0\controllers;
 
+use yii\base\Event;
 use thyseus\auth0\models\LoginForm;
 use Yii;
 
@@ -18,6 +19,14 @@ use Yii;
  */
 class UserController extends \yii\web\Controller
 {
+    const EVENT_BEFORE_AUTH0_LOGIN = 'event_before_auth0_login';
+
+    const EVENT_BEFORE_AUTH0_LOGOUT = 'event_before_auth0_logout';
+    const EVENT_AFTER_AUTH0_LOGOUT = 'event_after_auth0_logout';
+
+    const EVENT_BEFORE_AUTH0_LOGIN_CALLBACK = 'event_before_auth0_login_callback';
+    const EVENT_AFTER_AUTH0_LOGIN_CALLBACK = 'event_after_auth0_login_callback';
+
     /**
      * Login a user with auth0
      *
@@ -25,6 +34,8 @@ class UserController extends \yii\web\Controller
      */
     public function actionLogin()
     {
+        Event::trigger(self::class, self::EVENT_BEFORE_AUTH0_LOGIN);
+
         $auth0 = $this->module->auth0;
 
         if (! Yii::$app->user->isGuest) {
@@ -38,12 +49,15 @@ class UserController extends \yii\web\Controller
 
     public function actionCallback()
     {
+        Event::trigger(self::class, self::EVENT_BEFORE_AUTH0_LOGIN_CALLBACK);
+
         $auth0 = $this->module->auth0;
 
         $model = new LoginForm;
 
         if ($auth0->getUser()) {
             $model->login();
+            Event::trigger(self::class, self::EVENT_AFTER_AUTH0_LOGIN_CALLBACK);
             return $this->goBack();
         }
     }
@@ -55,8 +69,13 @@ class UserController extends \yii\web\Controller
      */
     public function actionLogout()
     {
+        Event::trigger(self::class, self::EVENT_BEFORE_AUTH0_LOGOUT);
+
         $this->module->auth0->logout();
-        Yii::$app->auth0user->logout();
+        Yii::$app->user->logout();
+
+        Event::trigger(self::class, self::EVENT_AFTER_AUTH0_LOGOUT);
+
         return $this->goHome();
     }
 }
